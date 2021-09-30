5d43f8b485c8193edf56e52536f1a89050df35bc
Baros2535 <brsaltn.25@gmail.com>
2021-09-30 22:59:47 +0300

    SameLetters.java

--- src/SameLetters.java
+++ src/SameLetters.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class SameLetters {
 
@@ -17,8 +14,47 @@ public class SameLetters {
         System.out.println("same(\"abs\",\"sba\") = " + same("abs", "sba"));
         System.out.println("same(\"Aab\",\"bAA\") = " + same("Aab", "bAA"));
 
+        removeDup("AAABBBCCC");
+        System.out.println("reverse(\"ABCD\") = " + reverse("ABCD"));
+        System.out.println("unique(\"AAABBBCCCDEF\") = " + unique("AAABBBCCCDEF"));
+
     }
 
+    //string reverse eden kod
+    private static String reverse(String string) {
+       //Collection kullanarak reverse
+        List<String> strings = Arrays.asList(string.split(""));
+        Collections.reverse(strings);
+
+        System.out.println("string = " + strings);
+        String dummy="";
+        for (int i = 0; i < string.length(); i++) {
+            dummy+=string.charAt(string.length()-i-1);
+        }
+
+return dummy;
+    }
+
+    //unıque değerleri bulan method
+    private static String unique(String string1) {
+        String result="";
+        Map<String, Integer> stringIntegerMap = fruqencyOfchars(string1);
+        for (Map.Entry<String, Integer> entry : stringIntegerMap.entrySet()) {
+            if (entry.getValue()==1){
+                result+=entry.getKey()+" ";
+            }
+
+        }
+return result;
+    }
+
+    //tekrar eden karekterleri atan kod
+    private static void removeDup(String string1) {
+        System.out.println("fruqencyOfchars(string1) = " + fruqencyOfchars(string1));
+        System.out.println("fruqencyOfchars(string1).keySet() = " + fruqencyOfchars(string1).keySet());
+    }
+
+    // stringin aynı harflerden oluşup oluşmadığını kontrol eden kod
     private static boolean same(String string1, String string2) {
         fruqencyOfchars(string1);
         fruqencyOfchars(string2);
@@ -30,7 +66,7 @@ public class SameLetters {
         }
 
     }
-
+// harflerin ne kadar sıklıkta kullandığını bulan kod
     public static  Map<String,Integer>fruqencyOfchars(String input) {
         String[] split = input.toLowerCase().split("");
 
@@ -38,7 +74,7 @@ public class SameLetters {
         Map<String,Integer> fruqMethod=new LinkedHashMap<>();
 
         for (String s : split) {
-
+        // Collections interface ine ait kod parçacığı
             fruq.put(s.toLowerCase(), Collections.frequency(Arrays.asList(split),s));
         }
 
@@ -50,6 +86,8 @@ public class SameLetters {
 
     }
 
+
+    //dizideki harflerin frekansını bulan method
     public static int fruqency(String s,String[] array){
         int count=0;
         for (String a:array){
